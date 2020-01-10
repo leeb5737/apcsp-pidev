1d66170a39e5573cd33531147f5ab85a52284c02
Bryton Lee <leeb5737@my.sduhsd.net>
2020-01-10 13:06:47 -0800

    Activity 4.3.2

--- student.c
+++ student.c
@@ -20,7 +20,28 @@ int main()
   char input[256];
   int p;
   struct Info safe[1000];
-  while (1)
+  int g = 1;
+  char count;
+  int realcount;
+  printf("Insert number of students\n");
+  while (g == 1)
+  {
+    fgets(input, 256, stdin);
+    sscanf(input, "%s", &count);
+    if (isdigit(count))
+    {
+      g = 0;
+      realcount = atoi(&count);
+      realcount = realcount;
+    }
+    else
+    {
+      printf("Invalid input, please plug in a number\n");
+      g = 1;
+    }
+
+  }
+  while (i != realcount)
     {
     printf("Insert student info (first last age id):\n");
     fgets(input, 256, stdin);
@@ -38,6 +59,18 @@ int main()
       i = i+1;
        }
     }
-
+  printf("Now, you can search for students by number (1 - %d).\n", realcount);
+  while (g == 0)
+  {
+    printf("Print student location (#)\n");
+    fgets(input, 256, stdin);
+    sscanf(input, "%s" "%s" "%d" "%d", student.first, student.last, &student.age, &student.id);
+    if (isdigit(*student.first))
+      {
+      p = atoi(student.first);
+      p = p -1 ;
+      printStudent(&safe[p]);
+      }
+  }
 }
 
